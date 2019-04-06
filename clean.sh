739f95e2fa3ffa1c8164db18789d3d06f6dba6b3
CompulsiveCoder <compulsivecoder@gmail.com>
2019-04-06 04:32:47 +0000

    Fix

--- clean.sh
+++ clean.sh
@@ -3,6 +3,8 @@ sh inject-version.sh "1-0-0-1"
 SLN_PROJ_DIR="src/Serial1602ShieldSystemUIController"
 
 if [ -f $SLN_PROJ_DIR/app.config.default ]; then
-  mv $SLN_PROJ_DIR/app.config $SLN_PROJ_DIR/app.config.security
-  mv $SLN_PROJ_DIR/app.config.default $SLN_PROJ_DIR/app.config
+  cp -f $SLN_PROJ_DIR/app.config.default $SLN_PROJ_DIR/app.config
+else
+  echo "Can't find default config file: $SLN_PROJ_DIR/app.config.default"
+  exit 1
 fi
