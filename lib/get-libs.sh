77b85e0de9d4b1289ebb73baee87494ad1e7d4c7
CompulsiveCoder <compulsivecoder@gmail.com>
2019-07-19 12:24:58 +1000

    Overhauled the way libraries are managed

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -1,10 +1,12 @@
 echo "Getting library files..."
 echo "  Dir: $PWD"
 
-sh install-package.sh NUnit 2.6.4
-sh install-package.sh NUnit.Runners 2.6.4
-sh install-package.sh Newtonsoft.Json 11.0.2
-sh install-package.sh duinocom.core 1.2.0.16
-sh install-package.sh M2Mqtt 4.3.0.0
+bash install-package-from-libs-repository.sh CompulsiveCoder NUnit 2.6.4 || exit 1
+bash install-package-from-libs-repository.sh CompulsiveCoder NUnit.Runners 2.6.4 || exit 1
+bash install-package-from-libs-repository.sh CompulsiveCoder Newtonsoft.Json 11.0.2 || exit 1
+bash install-package-from-libs-repository.sh CompulsiveCoder M2Mqtt 4.3.0.0 || exit 1
+bash install-package-from-libs-repository.sh CompulsiveCoder M2Mqtt 4.3.0.0 || exit 1
+
+bash install-package-from-github-release.sh CompulsiveCoder duinocom.core 1.2.0.25 || exit 1
 
 echo "Finished getting library files."
