5af5154fbf554e4894182dd9d138ea8ef407aef7
CompulsiveCoder <compulsivecoder@gmail.com>
2019-11-23 08:55:24 +1100

    Set Console.Title

--- src/Serial1602ShieldSystemUIController.ClientConsole/Program.cs
+++ src/Serial1602ShieldSystemUIController.ClientConsole/Program.cs
@@ -15,75 +15,73 @@ using System.Net.Mail;
 
 namespace Serial1602ShieldSystemUIController.ClientConsole
 {
-    class MainClass
+  class MainClass
+  {
+    public static bool IsSubscribed = false;
+    public static string IncomingKeyValueSeparator = "";
+    public static bool IsVerbose;
+
+    public static void Main (string[] args)
     {
-        public static bool IsSubscribed = false;
+      Console.Title = "GrowSenseSerialUIController";
+      
+      var arguments = new Arguments (args);
 
-        public static string IncomingKeyValueSeparator = "";
-
-        public static bool IsVerbose;
-
-        public static void Main (string[] args)
-        {
-            var arguments = new Arguments (args);
-
-            Run (arguments);
-        }
-
-        public static void Run (Arguments arguments)
-        {
-            var controller = new SystemMenuController ();
-
-            controller.MqttUsername = GetConfigValue (arguments, "UserId");
-            controller.MqttPassword = GetConfigValue (arguments, "Password");
-            controller.MqttHost = GetConfigValue (arguments, "Host");
-            controller.MqttPort = Convert.ToInt32 (GetConfigValue (arguments, "MqttPort"));
-
-
-            controller.SerialPortName = GetConfigValue (arguments, "SerialPort");
-            controller.SerialBaudRate = Convert.ToInt32 (GetConfigValue (arguments, "SerialBaudRate"));
-
-            controller.DeviceName = GetConfigValue (arguments, "DeviceName");
-
-            controller.EmailAddress = GetConfigValue (arguments, "EmailAddress");
-            controller.SmtpServer = GetConfigValue (arguments, "SmtpServer");
-
-            controller.DevicesDirectory = GetConfigValue (arguments, "DevicesDirectory");
-            controller.TargetDirectory = GetConfigValue (arguments, "TargetDirectory");
-
-            IsVerbose = arguments.Contains ("v");
-            controller.IsVerbose = IsVerbose;
-
-            controller.Run ();
-        }
-
-
-        public static string GetConfigValue (Arguments arguments, string argumentKey)
-        {
-            var value = String.Empty;
-
-            if (IsVerbose)
-                Console.WriteLine ("Getting config/argument value for: " + argumentKey);
-
-            if (arguments.Contains (argumentKey)) {
-                value = arguments [argumentKey];
-                if (IsVerbose)
-                    Console.WriteLine ("Found in arguments");
-            } else {
-
-                try {
-                    value = ConfigurationManager.AppSettings [argumentKey];
-                } catch (Exception ex) {
-                    Console.WriteLine ("Failed to get configuration value: " + argumentKey);
-                    throw ex;
-                }
-
-                if (IsVerbose)
-                    Console.WriteLine ("Looking in config");
-            }
-
-            return value;
-        }
-
+      Run (arguments);
     }
+
+    public static void Run (Arguments arguments)
+    {
+      var controller = new SystemMenuController ();
+
+      controller.MqttUsername = GetConfigValue (arguments, "UserId");
+      controller.MqttPassword = GetConfigValue (arguments, "Password");
+      controller.MqttHost = GetConfigValue (arguments, "Host");
+      controller.MqttPort = Convert.ToInt32 (GetConfigValue (arguments, "MqttPort"));
+
+
+      controller.SerialPortName = GetConfigValue (arguments, "SerialPort");
+      controller.SerialBaudRate = Convert.ToInt32 (GetConfigValue (arguments, "SerialBaudRate"));
+
+      controller.DeviceName = GetConfigValue (arguments, "DeviceName");
+
+      controller.EmailAddress = GetConfigValue (arguments, "EmailAddress");
+      controller.SmtpServer = GetConfigValue (arguments, "SmtpServer");
+
+      controller.DevicesDirectory = GetConfigValue (arguments, "DevicesDirectory");
+      controller.TargetDirectory = GetConfigValue (arguments, "TargetDirectory");
+
+      IsVerbose = arguments.Contains ("v");
+      controller.IsVerbose = IsVerbose;
+
+      controller.Run ();
+    }
+
+    public static string GetConfigValue (Arguments arguments, string argumentKey)
+    {
+      var value = String.Empty;
+
+      if (IsVerbose)
+        Console.WriteLine ("Getting config/argument value for: " + argumentKey);
+
+      if (arguments.Contains (argumentKey)) {
+        value = arguments [argumentKey];
+        if (IsVerbose)
+          Console.WriteLine ("Found in arguments");
+      } else {
+
+        try {
+          value = ConfigurationManager.AppSettings [argumentKey];
+        } catch (Exception ex) {
+          Console.WriteLine ("Failed to get configuration value: " + argumentKey);
+          throw ex;
+        }
+
+        if (IsVerbose)
+          Console.WriteLine ("Looking in config");
+      }
+
+      return value;
+    }
+  }
 }
